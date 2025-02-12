8efe7bffc1b8fd21d5ef80c622f2a4bfce5d68ee
Malik Hussein <malikhussein229@gmail.com>
2025-02-12 15:30:32 +0200

    Added routing to the website

--- products/src/App.jsx
+++ products/src/App.jsx
@@ -1,77 +1,27 @@
-import { useState } from 'react';
-import wdnstool from './assets/1.png';
-import gldnwtch from './assets/2.png';
-import gpu from './assets/3.png';
-import hrddsk from './assets/4.png';
-import ProductCard from './ProductCard';
+import Navbar from './components/Navbar/Navbar';
+import Category from './components/Category/Category';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 
 function App() {
-  const [products, setNewProducts] = useState([
-    { id: 1, title: 'Wooden Stool', price: 20, image: wdnstool },
-    { id: 2, title: 'Golden Watch', price: 100, image: gldnwtch },
-    { id: 3, title: 'Graphics Card', price: 150, image: gpu },
-    { id: 4, title: 'Hard Disk', price: 70, image: hrddsk },
+  const routes = createBrowserRouter([
+    {
+      path: '',
+      element: <Layout />,
+      children: [
+        { path: 'home', element: <Home /> },
+        { path: 'category', element: <Category /> },
+        { path: 'register', element: <Register /> },
+        { path: 'login', element: <Login /> },
+      ],
+    },
   ]);
-  const [searchValue, setNewSearch] = useState('');
-  console.log(searchValue);
-
-  function deleteProduct(id) {
-    let newProducts = structuredClone(products);
-
-    newProducts = newProducts.filter((product) => product.id != id);
-
-    setNewProducts(newProducts);
-  }
-
-  function increasePrice(id) {
-    let newProducts = structuredClone(products);
-
-    const newProduct = newProducts.find((product) => product.id == id);
-    newProduct.price += 10;
-
-    setNewProducts(newProducts);
-  }
-
-  function decreasePrice(id) {
-    let newProducts = structuredClone(products);
-
-    const newProduct = newProducts.find((product) => product.id == id);
-    newProduct.price -= 10;
-    if (newProduct.price < 0) {
-      newProduct.price = 0;
-    }
-
-    setNewProducts(newProducts);
-  }
-
-  const searchedProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
   return (
     <>
-      <div className="container">
-        <div className="d-flex my-5">
-          <input
-            type="text"
-            className="form-control me-3"
-            id="search"
-            placeholder="Search..."
-            onChange={(e) => setNewSearch(e.target.value)}
-          />
-        </div>
-        <div className="row" id="products">
-          {searchedProducts.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              deleteProduct={deleteProduct}
-              increasePrice={increasePrice}
-              decreasePrice={decreasePrice}
-            />
-          ))}
-        </div>
-      </div>
+      <RouterProvider router={routes}></RouterProvider>
     </>
   );
 }
--- products/src/components/Navbar/Navbar.jsx
+++ products/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
@@ -22,24 +23,32 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink className="nav-link" aria-current="page" to={'/home'}>
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to={'/category'}
+                >
                   Category
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to={'/register'}
+                >
                   Register
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <NavLink className="nav-link" aria-current="page" to={'/login'}>
                   Login
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
