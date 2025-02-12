753e096b68df7a46b25cde60efa9cd5cfdf0c6eb
Malik Hussein <malikhussein229@gmail.com>
2025-02-12 15:50:50 +0200

    Changed the ui of the card

--- products/src/components/ProductCard/ProductCard.jsx
+++ products/src/components/ProductCard/ProductCard.jsx
@@ -8,7 +8,7 @@ export default function ProductCard({
 }) {
   return (
     <>
-      <div className="col-3">
+      <div className="col-3 mb-4">
         <div className="card">
           <div className="square-image position-relative">
             <img src={product.image} alt="" />
@@ -17,37 +17,14 @@ export default function ProductCard({
             <p className="card-title text-nowrap overflow-hidden fw-bold h4 text-center">
               {product.title}
             </p>
-            <div className="d-flex justify-content-around align-items-center">
-              <button
-                className="btn btn-outline-primary rounded-start-4"
-                onClick={() => {
-                  decreasePrice(product.id);
-                }}
-              >
-                <i class="fa-solid fa-minus"></i>
-              </button>
-
+            <div className="d-flex justify-content-between align-items-center">
               <p className="card-title h5 mt-1">${product.price}</p>
-
-              <button
-                className="btn btn-outline-primary rounded-end-4"
-                onClick={() => {
-                  increasePrice(product.id);
-                }}
-              >
-                <i class="fa-solid fa-plus"></i>
-              </button>
+              <p className="card-title h5 mt-1">
+                {product.rating.rate}
+                <i className="fa-solid fa-star text-warning "></i>
+              </p>
             </div>
           </div>
-
-          <button
-            className="btn btn-danger mx-2 mb-2"
-            onClick={() => {
-              deleteProduct(product.id);
-            }}
-          >
-            Delete Product
-          </button>
         </div>
       </div>
     </>
