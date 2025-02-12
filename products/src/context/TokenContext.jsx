228f1df92be2a512ca78dddfb326be62a16fd878
Malik Hussein <malikhussein229@gmail.com>
2025-02-13 01:53:37 +0200

    Created a context to handle the token

---
+++ products/src/context/TokenContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+
+export let TokenContext = createContext();
+
+export default function TokenContextProvider({ children }) {
+  const [token, setToken] = useState();
+
+  return (
+    <TokenContext.Provider value={{ token, setToken }}>
+      {children}
+    </TokenContext.Provider>
+  );
+}
