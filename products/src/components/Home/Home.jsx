7de3011bee9e5f97e47387b7cbaf620e100f1634
Malik Hussein <malikhussein229@gmail.com>
2025-02-13 14:18:18 +0200

    Implemented the pagination

--- products/src/components/Home/Home.jsx
+++ products/src/components/Home/Home.jsx
@@ -1,58 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductCard from '../ProductCard/ProductCard';
 import { BeatLoader } from 'react-spinners';
+import { PaginationContext } from '../../context/PaginationContext';
+import Pagination from '../Pagination/Pagination';
 
 export default function () {
-  const [products, setProducts] = useState([]);
-
-  async function getProducts() {
-    try {
-      let { data } = await axios.get('https://fakestoreapi.com/products');
-      console.log(data);
-      setProducts(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const [searchValue, setSearchValue] = useState('');
-  console.log(searchValue);
-
-  const searchedProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchValue.toLowerCase())
-  );
+  let { paginatedProducts } = useContext(PaginationContext);
 
   return (
     <>
       <div className="container">
-        <div className="d-flex my-5">
-          <input
-            type="text"
-            className="form-control me-3"
-            id="search"
-            placeholder="Search..."
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
-        </div>
-        {products.length == 0 ? (
-          <div className="d-flex my-5">
-            <BeatLoader speedMultiplier={0.7} className="mx-auto" />
-          </div>
-        ) : (
-          <>
-            <div className="row" id="products">
-              {searchedProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
+        <div className="my-5">
+          {paginatedProducts.length == 0 ? (
+            <div className="d-flex my-5">
+              <BeatLoader speedMultiplier={0.7} className="mx-auto" />
             </div>
-          </>
-        )}
+          ) : (
+            <>
+              <div className="row" id="products">
+                {paginatedProducts.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+
+        <Pagination />
       </div>
     </>
   );
--- products/src/main.jsx
+++ products/src/main.jsx
@@ -4,11 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App.jsx';
 import TokenContextProvider from './context/TokenContext.jsx';
+import PaginationContextProvider from './context/PaginationContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TokenContextProvider>
-      <App />
+      <PaginationContextProvider>
+        <App />
+      </PaginationContextProvider>
     </TokenContextProvider>
   </StrictMode>
 );
